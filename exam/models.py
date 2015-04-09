232183e52a43086655966c301d70db2c982b2eba
Alexey Kutepov <alkutepov@gmail.com>
2015-04-09 18:38:13 +0400

    Create test function was added

--- exam/models.py
+++ exam/models.py
@@ -135,7 +135,7 @@ class Test(models.Model):
     # The description of test
     description = models.TextField(blank=True)
     # The JSON of the test
-    test = models.TextField(blank=True)
+    test = models.BinaryField(blank=True)
     # The author of the test
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     # The category of the test
--- exam/views.py
+++ exam/views.py
@@ -1,7 +1,9 @@
+from django.core.exceptions import SuspiciousOperation
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from exam.models import Category, Test
+from exam.models import Category, Test, TestImage
 from django.http import HttpResponseRedirect
+import pickle
 
 
 def index(request):
@@ -32,7 +34,7 @@ def create_new_test(request):
             author=request.user,
             is_public=is_public,
         )
-        return HttpResponseRedirect("/create_new_question/?id=" + str(test.id) + "/")
+        return HttpResponseRedirect("/create_new_question/" + str(test.id) + "/")
     else:
         category_list = Category.objects.all()
         return render(request, "exam/create_new_test.html", {"category_list": category_list})
@@ -40,9 +42,66 @@ def create_new_test(request):
 
 @login_required(login_url='/')
 def create_new_question(request, id):
+    """
+    Creates new question of the test of id
+    :param request:
+    :param id: id of the test
+    :return:
+    """
+    test = Test.objects.get(id=id)
+    if request.user != test.author:
+        raise SuspiciousOperation("Некорректный id теста")
     type_list = [
-        "Содержит один или несколько правильных вариантов ответа",
-        "Содержит только один правильный вариант ответа",
-        "Вопрос со свободной формой ответа"
-    ]
-    return render(request, "exam/create_new_question.html", {"type_list": type_list, "test_id": id})
+            "Содержит один или несколько правильных вариантов ответа",
+            "Содержит только один правильный вариант ответа",
+            "Вопрос со свободной формой ответа"
+        ]
+    if "type" in request.POST and int(request.POST["type"]) in (1, 2, 3):
+        if test.test is None or test.test == b'':
+            json_test = []
+        else:
+            json_test = pickle.loads(test.test)
+        question_type = int(request.POST["type"])
+        image = TestImage.objects.get_or_create(image=request.FILES["image"])
+        answers = None
+        if question_type == 1:
+            answers = []
+            i = 1
+            while ("answer"+str(i) in request.POST):
+                answers.append({
+                    "answer": request.POST["answer"+str(i)],
+                    "is_correct": str(i) in request.POST.getlist("trueAnswer")
+                })
+                i += 1
+        elif question_type == 2:
+            pass
+        elif question_type == 3:
+            answers = request.POST["openAnswer"]
+        question = {
+            "number_of_question": len(json_test) + 1,
+            "question": request.POST["question"],
+            "type": question_type,
+            "image": image[0].id,
+            "answers": answers
+        }
+        json_test.append(question)
+        test.test = pickle.dumps(json_test)
+        test.save()
+        return render(
+            request,
+            "exam/create_new_question.html",
+            {
+                "number_of_question": len(json_test) + 1,
+                "type_list": type_list,
+                "test_id": id
+            }
+        )
+    else:
+        return render(
+            request,
+            "exam/create_new_question.html",
+            {
+                "type_list": type_list,
+                "test_id": id
+            }
+        )
